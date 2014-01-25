1e20ca2c9f9e7a26d63e2ca142a58d5bec9561f5
Justin Hamilton <hamilton@bizo.com>
2014-01-25 20:36:49 +0100

    Adding scripts for the manipulation of water rings

---
+++ Assets/Scripts/Water/Stretcher.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stretcher : MonoBehaviour {
+	public Vector3 stretchTime;
+	private Transform myTransform;
+	private Vector3 newScale;
+
+	// Use this for initialization
+	void Start () {
+		myTransform = transform;
+		newScale = myTransform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		newScale = newScale + stretchTime * Time.deltaTime;
+		myTransform.localScale = newScale;
+	}
+}
---
+++ Assets/Scripts/Water/TransparentDestroyer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransparentDestroyer : MonoBehaviour {
+
+	private Material material;
+
+	// Use this for initialization
+	void Start () {
+		material = renderer.material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (material.color.a <= 0) {
+			Destroy(gameObject);
+		}
+	}
+}

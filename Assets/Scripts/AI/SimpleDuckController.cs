800f11b6a95bb160aa58fa7b733b0c09327d11bf
Justin Hamilton <hamilton@bizo.com>
2014-01-25 22:07:18 +0100

    Very basic back-and-forth duck controller

---
+++ Assets/Scripts/AI/SimpleDuckController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(tk2dSprite))]
+public class SimpleDuckController : MonoBehaviour {
+
+	public float xRangeFromStart;
+	public float movementSpeed;
+	public float turnDelay;
+	public Direction direction;
+
+	public enum Direction {
+		RANDOM, LEFT, RIGHT,
+	}
+
+	private bool turning;
+	private float turnTime;
+	private Rigidbody2D rBody;
+	private tk2dSprite sprite;
+	private Transform myTransform;
+	private Vector3 startingPos;
+	
+	// Use this for initialization
+	void Start () {
+		sprite = GetComponent<tk2dSprite> ();
+		rBody = rigidbody2D;
+		myTransform = transform;
+		startingPos = myTransform.position;
+		if (direction == Direction.RANDOM) {
+			direction = (Direction) Random.Range(1, 3);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (direction == Direction.LEFT && !turning) {
+			rBody.AddForce (new Vector2 (movementSpeed * -1, 0));
+			sprite.FlipX = true;
+			if (myTransform.position.x <= startingPos.x - xRangeFromStart) turning = true;
+		} else if (direction == Direction.RIGHT && !turning) {
+			rBody.AddForce (new Vector2 (movementSpeed, 0));
+			sprite.FlipX = false;
+			if (myTransform.position.x > startingPos.x + xRangeFromStart) turning = true;
+		} else if (turning) {
+			rBody.velocity = Vector3.zero;
+			turnTime += Time.deltaTime;
+
+			if (turnTime >= turnDelay) {
+				turnTime = 0;
+				turning = false;
+				if (direction == Direction.LEFT) {
+					direction = Direction.RIGHT;
+				} else {
+					direction = Direction.LEFT;
+				}
+			}
+		}
+	}
+}

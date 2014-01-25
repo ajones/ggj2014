56d3aef0f98faed42c5e4f348f177a0c1f62724b
Justin Hamilton <hamilton@bizo.com>
2014-01-25 07:55:23 +0100

    Implementing camera follow and zoom

---
+++ Assets/Scripts/Camera/FollowController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowController : MonoBehaviour {
+
+	public GameObject target;
+
+	// variables for caching
+	private Transform myTransform;
+	private Transform targetTransform;
+
+
+	// Use this for initialization
+	void Start () {
+		myTransform = transform;
+		targetTransform = target.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPosition = new Vector3 (targetTransform.position.x, targetTransform.position.y, myTransform.position.z);
+		myTransform.position = newPosition;
+	}
+}
---
+++ Assets/Scripts/Camera/ZoomController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoomController : MonoBehaviour, IEventListener {
+
+	// we may want to have this be data associated with the event, but right now this works
+	public float inSize;
+	public float outSize;
+	public float moveSpeed;
+
+	private Camera cam;
+
+	void Awake() {
+		EventManager.AddListener(this, "LevelStart");
+		EventManager.AddListener(this, "Segway");
+	}
+
+	// Use this for initialization
+	void Start() {
+		cam = Camera.main;
+		EventManager.TriggerEventAfter (new LevelStart (), 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	bool IEventListener.HandleEvent(IEvent evt) {
+		switch (evt.GetName()) {
+		case "LevelStart":
+			StartCoroutine("ZoomIn");
+			break;
+		case "Segway":
+			StartCoroutine("ZoomOut");
+			break;
+		default:
+			Debug.Log ("Default!");
+			break;
+		}        
+		return false;
+	}
+
+	IEnumerator ZoomIn() {
+		float originalSize = cam.orthographicSize;
+		float diffInSize = originalSize - inSize;
+		float currentDiff = 0;
+		while (cam.orthographicSize > inSize) {
+			currentDiff = Mathf.Lerp (currentDiff, diffInSize, moveSpeed * Time.deltaTime);
+			cam.orthographicSize = originalSize - currentDiff;
+			yield return new WaitForEndOfFrame();
+		}
+	}
+
+	IEnumerator ZoomOut() {
+		while (cam.orthographicSize < outSize) {
+			cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, outSize, moveSpeed * Time.deltaTime);
+			yield return new WaitForEndOfFrame();
+		}
+	}
+}

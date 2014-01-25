88413da33392e82d71824e6cde105cd717463a23
Aaron Jones <r.aaron.jones@gmail.com>
2014-01-24 20:54:34 -0800

    removed demo scenes. added base movement

---
+++ Assets/Scripts/Character/MovementController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour {
+
+	float movementSpeed = 500f;
+	float jumpSpeed = 10000f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("up")){
+			this.rigidbody.AddForce(new Vector3(0,jumpSpeed,0));
+		}
+		if (Input.GetKey ("right")){
+			this.rigidbody.AddForce(new Vector3(movementSpeed,0,0));
+			this.transform.localScale = new Vector3(
+				Mathf.Abs (this.transform.localScale.x),
+				this.transform.localScale.y,
+				this.transform.localScale.z);
+		}
+		if (Input.GetKey ("left")){
+			this.rigidbody.AddForce(new Vector3(movementSpeed * -1,0,0));
+			this.transform.localScale = new Vector3(
+				Mathf.Abs (this.transform.localScale.x) * -1,
+				this.transform.localScale.y,
+				this.transform.localScale.z);
+		}
+	}
+}

d6fa4d98bdffa00a2e88ec9125b53f3c86ad6553
Justin Hamilton <hamilton@bizo.com>
2014-01-25 18:06:06 +0100

    Adding basic give to the camera follow

--- Assets/Scripts/Camera/FollowController.cs
+++ Assets/Scripts/Camera/FollowController.cs
@@ -4,21 +4,47 @@ using System.Collections;
 public class FollowController : MonoBehaviour {
 
 	public GameObject target;
+	public float xGive;
+	public float yGive;
 
 	// variables for caching
-	private Transform myTransform;
+	private Transform camTransform;
 	private Transform targetTransform;
-
+	private Vector3 newCamPosition;
 
 	// Use this for initialization
 	void Start () {
-		myTransform = transform;
+		camTransform = Camera.main.transform;
 		targetTransform = target.transform;
+		newCamPosition = new Vector3(targetTransform.position.x, targetTransform.position.y, camTransform.position.z);
+		camTransform.position = newCamPosition;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 newPosition = new Vector3 (targetTransform.position.x, targetTransform.position.y, myTransform.position.z);
-		myTransform.position = newPosition;
+		float xDiff;
+		float yDiff;
+
+		if (targetTransform.position.x < camTransform.position.x - xGive) {
+			xDiff = (camTransform.position.x - xGive) - targetTransform.position.x; 
+			newCamPosition.x -= xDiff;
+			camTransform.position = newCamPosition;
+		} else if (targetTransform.position.x > camTransform.position.x + xGive) {
+			xDiff = targetTransform.position.x - (camTransform.position.x + xGive);
+			newCamPosition.x += xDiff;
+			camTransform.position = newCamPosition;
+		}
+
+		if (targetTransform.position.y < camTransform.position.y - yGive) {
+			yDiff = (camTransform.position.y - yGive) - targetTransform.position.y;
+			newCamPosition.y -= yDiff;
+			camTransform.position = newCamPosition;
+		} else if (targetTransform.position.y > camTransform.position.y + yGive) {
+			yDiff = targetTransform.position.y - (camTransform.position.y + yGive);
+			newCamPosition.y += yDiff;
+			camTransform.position = newCamPosition;
+    	}
+		
 	}
+	
 }

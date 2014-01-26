d18f8a3deb9310549cce82f35261f77a39a8a48a
Aaron Jones <r.aaron.jones@gmail.com>
2014-01-25 22:33:50 -0800

    new back ground and fade controller

---
+++ Assets/Scripts/Door/FadeController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeController : MonoBehaviour {
+
+	public tk2dSprite spriteToFade;
+
+
+	public void FadeIn() {
+		StopCoroutine("FadeTo");
+		StartCoroutine("FadeTo",1.0f);
+	}
+
+	public void FadeOut() {
+		StopCoroutine("FadeTo");
+		StartCoroutine("FadeTo",0.0f);
+	}
+
+	IEnumerator FadeTo(float targetAlpha){
+		float accum = 0f;
+
+		Color startingColor = spriteToFade.color;
+		while (accum < 1f) {
+			spriteToFade.color = new Color(startingColor.r,startingColor.g,startingColor.b,Mathf.Lerp(startingColor.a,targetAlpha,accum));
+			accum += Time.deltaTime;
+			yield return new WaitForEndOfFrame();	
+		}
+	}
+}
--- Assets/Scripts/Interaction/CameraInteractionTarget.cs
+++ Assets/Scripts/Interaction/CameraInteractionTarget.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class CameraInteractionTarget : InteractionTarget {
 
+	public FadeController fadeController;
+
 	public override void beginInteraction (string id) {
 		
 		EventManager.TriggerEvent(new LevelStart());
+		fadeController.FadeOut();
 	}
 	
 	public override void stopInteraction (string id) {
 		EventManager.TriggerEvent(new Segway());
+		fadeController.FadeIn();
 	}
 	
 	

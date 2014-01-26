1a54727bd3c6769741a826f765a3581c038d6303
Justin Hamilton <hamilton@bizo.com>
2014-01-26 06:42:00 +0100

    Making babies leave

---
+++ Assets/Scripts/Babies/Leaver.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(tk2dSprite))]
+public class Leaver : MonoBehaviour {
+
+	public float speed;
+	
+	private tk2dSprite sprite;
+
+	void Start() {
+		sprite = GetComponent<tk2dSprite> ();
+	}
+
+	public void Leave() {
+		sprite.FlipX = true;
+		StartCoroutine ("ReallyLeave");
+	}
+
+	IEnumerator ReallyLeave() {
+		while (true) {
+			transform.Translate(speed * Time.deltaTime, 0, 0);
+			yield return null;
+		}
+	}
+}
